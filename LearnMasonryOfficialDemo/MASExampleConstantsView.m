d82a135c9c91e336a743468562f39305e2524306
github641 <mrliuzy@outlook.com>
2017-09-18 11:59:19 +0800

    约束公式中常数的使用，可以传入NSNumber和CGPoint，CGSize的类型；在等间距使用上，可以使用for循环，并使用变量记录下上一个视图的方法来做相对布局。

--- LearnMasonryOfficialDemo/MASExampleConstantsView.m
+++ LearnMasonryOfficialDemo/MASExampleConstantsView.m
@@ -13,7 +13,43 @@
 - (id)init {
     self = [super init];
     if (!self) return nil;
-        return self;
+    
+    UIView *purpleView = [UIView new];
+    purpleView.backgroundColor = [UIColor redColor];
+    purpleView.layer.borderWidth = 2;
+    [self addSubview:purpleView];
+    
+    UIView *yellowView = [UIView new];
+    yellowView.backgroundColor = [UIColor yellowColor];
+
+    yellowView.layer.borderWidth = 2;
+    [self addSubview:yellowView];
+    
+    
+    [purpleView mas_makeConstraints:^(MASConstraintMaker *make){
+//        make.top.left.bottom.right.equalTo(self).insets(UIEdgeInsetsMake(10, 10, 10, 10));
+        make.top.equalTo(@20);
+        make.left.equalTo(@20);
+        make.bottom.equalTo(@-20);
+        make.right.equalTo(@-20);
+    }];
+    
+    [yellowView mas_makeConstraints:^(MASConstraintMaker *make){
+        make.center.equalTo(CGPointMake(0, 50));
+        make.size.equalTo(CGSizeMake(300, 100));
+    }];
+
+
+    /* lzy170918注:
+     这个例子，只看界面和名字的话，没有明白想示范什么。
+     所以直接看源码。
+     是想举例 约束公式中的 constant的传入。
+     
+     传入 number类型，
+     传入CGPoint 、 CGSize的类型。
+     */
+    
+    return self;
 }
 
 @end
--- LearnMasonryOfficialDemo/MASExampleSidesView.m
+++ LearnMasonryOfficialDemo/MASExampleSidesView.m
@@ -13,8 +13,40 @@
 - (id)init {
     self = [super init];
     if (!self) return nil;
+    
+    UIView *lastView = self;
+    for (NSInteger i = 0; i < 10; i++) {
+        UIView *v = [UIView new];
+        [self addSubview:v];
+        v.backgroundColor = [self randomColor];
+        
+//        if (lastView) {
+//            [v mas_makeConstraints:^(MASConstraintMaker *make){
+//                make.top.left.bottom.right.equalTo(lastView).insets(UIEdgeInsetsMake(3 + i, 6 + i, 9 + i, 12 + i));
+//            }];
+//        }else{// 说明是第一个
+//            [v mas_makeConstraints:^(MASConstraintMaker *make){
+//                make.top.left.bottom.right.equalTo(self).insets(UIEdgeInsetsMake(3, 6, 9, 12));
+//            }];
+//
+//        }
+        /* lzy170918注:
+         之前的每次都多了 判断是否有 lastView的情况，其实初始化时设置为self，之后每创建一个就修改一次lastView就好了。
+         */
+        [v mas_makeConstraints:^(MASConstraintMaker *make){
+            make.top.left.bottom.right.equalTo(lastView).insets(UIEdgeInsetsMake(3 + i, 6 + i, 9 + i, 12 + i));
+        }];
+
+
+        lastView  = v;
+    }
+    
         return self;
 }
 
+- (UIColor *)randomColor{
+    
+    return  [UIColor colorWithRed:arc4random() % 255 / 255.0 green:arc4random() % 255 / 255.0 blue:arc4random() % 255 / 255.0 alpha:1];
+}
 
 @end
