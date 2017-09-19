7d60533f735878b34314408eac9802afeb3c3021
刘志意 <liuzhiyi@alldk.com>
2017-09-19 10:57:29 +0800

    自己简单实现的basic animation。不完善：起始位置不对；动画缩放没有保持间距比例，偏移了位置；横屏不行

--- LearnMasonryOfficialDemo/MASExampleAnimatedView.m
+++ LearnMasonryOfficialDemo/MASExampleAnimatedView.m
@@ -9,8 +9,13 @@
 #import "MASExampleAnimatedView.h"
 
 @interface MASExampleAnimatedView ()
+@property (nonatomic, strong)UIView *greenView;
 
+@property (nonatomic, strong)UIView *redView;
 
+@property (nonatomic, strong)UIView *blueView;
+
+@property (nonatomic, assign)BOOL bigPadding;
 @end
 
 @implementation MASExampleAnimatedView
@@ -19,8 +24,74 @@
     self = [super init];
     if (!self) return nil;
 
+    self.greenView = [UIView new];
+    [self addSubview:self.greenView];
+    self.greenView.backgroundColor = [UIColor greenColor];
+    
+    self.redView = [UIView new];
+    [self addSubview:self.redView];
+    self.redView.backgroundColor = [UIColor redColor];
+    
+    self.blueView = [UIView new];
+    [self addSubview:self.blueView];
+    self.blueView.backgroundColor = [UIColor blueColor];
+    
+    MASAttachKeys(self.greenView, self.blueView, self.redView);
+
+    self.bigPadding = NO;
+    [self change];
     
     return self;
+}
+- (void)change{
+
+    [self.greenView mas_remakeConstraints:^(MASConstraintMaker *make){
+        make.top.and.left.equalTo(self).offset(100 * self.bigPadding + 30).priorityLow();
+        
+    }];
+    
+    [self.redView mas_remakeConstraints:^(MASConstraintMaker *make){
+        make.top.equalTo(self).offset(100 * self.bigPadding + 30);
+        make.right.equalTo(self).offset(-(100 * self.bigPadding + 30));
+        
+        make.left.equalTo(self.greenView.mas_right).offset(100 * self.bigPadding + 10);
+        
+        make.size.equalTo(self.greenView);// 等价与下面两句
+        //        make.height.equalTo(greenView.mas_height);
+        //        make.width.equalTo(greenView.mas_width);
+        
+        make.height.equalTo(self.redView.mas_width).multipliedBy(1.5);
+    }];
+    
+    [self.blueView mas_remakeConstraints:^(MASConstraintMaker *make){
+        make.left.equalTo(self).offset(100 * self.bigPadding + 30);
+        make.right.bottom.equalTo(self).offset(-(100 * self.bigPadding + 30));
+        
+        make.top.lessThanOrEqualTo(self.greenView.mas_bottom).offset(100 * self.bigPadding + 10);
+    }];
+    
+    
+    
+    [self setNeedsUpdateConstraints];
+    
+//    [self updateConstraintsIfNeeded];
+    
+    self.bigPadding = !self.bigPadding;
+}
+
+- (void)updateConstraints{
+    
+    
+    
+//    [self setNeedsLayout];
+    
+    [UIView animateWithDuration:1 animations:^{
+        [self layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        [self change];
+    }];
+    
+    [super updateConstraints];
 }
 
 @end
--- LearnMasonryOfficialDemo/MASExampleAspectFitView.m
+++ LearnMasonryOfficialDemo/MASExampleAspectFitView.m
@@ -9,7 +9,10 @@
 #import "MASExampleAspectFitView.h"
 
 @interface MASExampleAspectFitView ()
-
+@property UIView *topView;
+@property UIView *topInnerView;
+@property UIView *bottomView;
+@property UIView *bottomInnerView;
 @end
 
 @implementation MASExampleAspectFitView
@@ -21,7 +24,117 @@
     
     if (self) {
         
-            }
+        /* lzy170918注:观察了一下demo界面，任务是
+         两层，一层是大view，两个容器视图将屏幕分为两半；
+         容器视图内有一个子视图，子视图的宽高比固定，宽或者高与父视图同；
+         
+         e.g.代码中，我是约束子视图的左右与父视图的左右间距为0，以此确定子视图宽 == 父视图宽，然后子视图的高与父视图的宽建立multipliedBy关系。
+         
+         看了下demo的实现代码：
+         1、视图层级是对的，都是2容器视图，以及容器视图内有一个子视图
+         2、子视图的宽高比，是自己对自己的约束，即自己的宽和自己的高建立multipliedBy关系
+         3、比我多的约束：子视图的宽高 不大于 父视图的宽高；并设置子视图宽高等于 父视图的宽高的约束优先级 为priorityLow()
+         
+         UIView *v1 = [UIView new];
+         [self addSubview:v1];
+         v1.backgroundColor = [UIColor orangeColor];
+         
+         UIView *v2 = [UIView new];
+         [v1 addSubview:v2];
+         v2.backgroundColor = [UIColor cyanColor];
+         
+         UIView *v3 = [UIView new];
+         [self addSubview:v3];
+         v3.backgroundColor = [UIColor greenColor];
+         
+         UIView *v4 = [UIView new];
+         [v3 addSubview:v4];
+         v4.backgroundColor = [UIColor yellowColor];
+         
+         [v1 mas_makeConstraints:^(MASConstraintMaker *make){
+         make.top.left.right.equalTo(self).offset(0);
+         // 可以简写：make.top.left.right.equalTo(self);
+         make.height.equalTo(self.height).multipliedBy(0.5);
+         }];
+         
+         [v3 mas_makeConstraints:^(MASConstraintMaker *make){
+         make.bottom.left.right.equalTo(self).offset(0);
+         make.height.equalTo(self.height).multipliedBy(0.5);
+         
+         }];
+         
+         [v2 mas_makeConstraints:^(MASConstraintMaker *make){
+         make.left.right.equalTo(v1).offset(0);
+         make.height.equalTo(v1.width).multipliedBy(0.15);
+         make.center.equalTo(v1.center);
+         }];
+         //
+         [v4 mas_makeConstraints:^(MASConstraintMaker *make){
+         make.top.bottom.equalTo(v3).offset(0);
+         make.width.equalTo(v3.height).multipliedBy(0.3);
+         make.center.equalTo(v3.center);
+         
+         }];
+         */
+
+        // Create views
+        self.topView = [[UIView alloc] initWithFrame:CGRectZero];
+        self.topInnerView = [[UIView alloc] initWithFrame:CGRectZero];
+        self.bottomView = [[UIView alloc] initWithFrame:CGRectZero];
+        self.bottomInnerView = [[UIView alloc] initWithFrame:CGRectZero];
+        
+        // Set background colors
+        UIColor *blueColor = [UIColor colorWithRed:0.663 green:0.796 blue:0.996 alpha:1];
+        [self.topView setBackgroundColor:blueColor];
+        
+        UIColor *lightGreenColor = [UIColor colorWithRed:0.784 green:0.992 blue:0.851 alpha:1];
+        [self.topInnerView setBackgroundColor:lightGreenColor];
+        
+        UIColor *pinkColor = [UIColor colorWithRed:0.992 green:0.804 blue:0.941 alpha:1];
+        [self.bottomView setBackgroundColor:pinkColor];
+        
+        UIColor *darkGreenColor = [UIColor colorWithRed:0.443 green:0.780 blue:0.337 alpha:1];
+        [self.bottomInnerView setBackgroundColor:darkGreenColor];
+        
+        // Layout top and bottom views to each take up half of the window
+        
+        [self addSubview:self.topView];
+        
+        [self.topView mas_makeConstraints:^(MASConstraintMaker *make){
+            make.left.right.and.top.equalTo(self);
+        }];
+        
+        [self addSubview:self.bottomView];
+        
+        [self.bottomView mas_makeConstraints:^(MASConstraintMaker *make){
+            make.left.right.and.bottom.equalTo(self);
+            make.top.equalTo(self.topView.mas_bottom);
+            make.height.equalTo(self.topView);
+        }];
+
+// Inner views are configured for aspect fit with ratio of 3:1
+        
+        [self.topView addSubview:self.topInnerView];
+        [self.topInnerView mas_makeConstraints:^(MASConstraintMaker *make){
+            make.width.equalTo(self.topInnerView.mas_height).multipliedBy(3);
+            make.width.and.height.lessThanOrEqualTo(self.topView);
+            make.width.and.height.equalTo(self.topView).width.priorityLow();
+            
+            make.center.equalTo(self.topView);
+        }];
+
+        [self.bottomView addSubview:self.bottomInnerView];
+        
+        [self.bottomInnerView mas_makeConstraints:^(MASConstraintMaker *make){
+            make.height.equalTo(self.bottomInnerView.mas_width).multipliedBy(3);
+            make.width.and.height.lessThanOrEqualTo(self.bottomView);
+            make.width.and.height.equalTo(self.bottomView).with.priorityLow();
+            
+            make.center.equalTo(self.bottomView);
+        }];
+
+        
+    }
     
     return self;
 }
